8a602d753d612b4969c1ca864fe7d0c90f48108d
shahezadk <61517014+shahezadk@users.noreply.github.com>
2020-06-15 16:26:58 +0530

    hi this is my maven auto build project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!This is my first manual maven project!! This is my first Maven auto build job" );
+        System.out.println( "Hello World!!" );
     }
 }
